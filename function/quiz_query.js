c65de90c43bf4cd0298090ac208864cd4210cd79
shadowverseCQ <qkdpdhfkznak@naver.com>
2018-07-19 18:38:34 +0900

    삽질의 끝이 보인다?

--- function/quiz_query.js
+++ function/quiz_query.js
@@ -1,3 +1,29 @@
 //여기는 퀴즈할때 쓸 함수를 넣으면됩니다.
-
+const axios = require('../node_modules/axios');
+const getData = () => {
+    let Paramcount = Math.floor((Math.random() * 8) + 0);
+    axios({
+        method: 'get',
+        url: 'https://shadowverse-portal.com/api/v1/cards',
+        params: {
+            'format': 'json',
+            'lang': 'ko',
+            'clan': Paramcount
+        }
+    }).then((res) => {
+        console.log(res);
+        //console.log(res.data.data.cards.length)
+        let count = Math.floor((Math.random() * res.data.data.cards.length) + 1);
+        //console.log(count);
+        //console.log(res.data.data.cards[count])
+        let correctCard = res.data.data.cards[count].card_name;
+        console.log(res.data.data.cards[count].card_name);
+        let cardText = res.data.data.cards[count].skill_disc;
+        let cardCost = res.data.data.cards[count].cost;
+        let cardATKLIFE = res.data.data.cards[count].atk + '/' + res.data.data.cards[count].life;
+        if(cardATKLIFE == '0/0'){
+            cardATKLIFE = '주문/마법진'
+        }
+    });
+}
 
