7ce53abd9634ed491f1eec7408701d648b213bf3
murikubo <skyaria2017@gmail.com>
2018-07-19 15:58:04 +0900

    삽질

--- function/essential.js
+++ function/essential.js
@@ -1,1 +1,27 @@
-//여기는 페이지 이동같은 평소에 쓰는 것을 넣으면 됩니다.
+const axios = require('../node_modules/axios');
+const getData = () => {
+    let Paramcount = Math.floor((Math.random() * 8) + 0);
+    axios({
+        method: 'get',
+        url: 'https://shadowverse-portal.com/api/v1/cards',
+        params: {
+            'format': 'json',
+            'lang': 'ko',
+            'clan': Paramcount
+        }
+    }).then((res) => {
+        console.log(res);
+        //console.log(res.data.data.cards.length)
+        let count = Math.floor((Math.random() * res.data.data.cards.length) + 1);
+        //console.log(count);
+        //console.log(res.data.data.cards[count])
+        let correctCard = res.data.data.cards[count].card_name;
+        console.log(res.data.data.cards[count].card_name);
+        let cardText = res.data.data.cards[count].skill_disc;
+        let cardCost = res.data.data.cards[count].cost;
+        let cardATKLIFE = res.data.data.cards[count].atk + '/' + res.data.data.cards[count].life;
+        if(cardATKLIFE == '0/0'){
+            cardATKLIFE = '주문/마법진'
+        }
+    });
+}
